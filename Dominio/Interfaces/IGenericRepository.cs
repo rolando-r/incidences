6a50b2d2d1b821cbf1fa1ce816fa5feb4b7c9161
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-23 09:26:10 -0500

    feat: :construction: Interfaces
    Constructing Interfaces

---
+++ Dominio/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+namespace Dominio.Interfaces;
+public interface IGenericRepository<T> where T : BaseEntity
+{
+        Task<T> GetByIdAsync(string id);
+        Task<IEnumerable<T>> GetAllAsync();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize,string search);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);        
+}
