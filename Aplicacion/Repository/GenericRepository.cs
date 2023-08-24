3524f57b2e7fe2b1321f06151cf96454e218c0c1
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-23 20:57:35 -0500

    feat: :construction: Repositories
    BaseRepository

---
+++ Aplicacion/Repository/GenericRepository.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Dominio;
+using Dominio.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
+
+namespace Aplicacion.Repository;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly IncidencesContext _context;
+
+    public GenericRepository(IncidencesContext context)
+    {
+        _context = context;
+    }
+
+    public virtual void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public virtual void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
+
+    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public Task<T> GetByIdAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Remove(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public virtual void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _context.Set<T>()
+            .Update(entity);
+    }
+    public virtual async Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize, string _search)
+    {
+        var totalRegistros = await _context.Set<T>().CountAsync();
+        var registros = await _context.Set<T>()
+            .Skip((pageIndex - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+        return (totalRegistros, registros);
+    }
+}
