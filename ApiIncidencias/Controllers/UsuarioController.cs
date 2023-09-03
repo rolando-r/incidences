25863440d545af7ca0ec787eab5bff1eb9d9dbbf
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-09-02 23:13:08 -0500

    feat: :sparkles: JWT
    in construction

--- ApiIncidencias/Controllers/UsuarioController.cs
+++ ApiIncidencias/Controllers/UsuarioController.cs
@@ -1,6 +1,71 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ApiIncidencias.Dtos;
+using ApiIncidencias.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
 namespace ApiIncidencias.Controllers;
 
-public class UsuarioController
+public class UsuariosController : BaseApiController
 {
-    
+    private readonly IUserService _userService;
+    public UsuariosController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterAsync(RegisterDto model)
+    {
+        var result = await _userService.RegisterAsync(model);
+        return Ok(result);
+    }
+
+    [HttpPost("token")]
+    public async Task<IActionResult> GetTokenAsync(LoginDto model)
+    {
+        var result = await _userService.GetTokenAsync(model);
+        return Ok(result);
+    }
+
+/*     [HttpPost]
+    [Route("Validar")]
+    public  IActionResult Validar([FromBody] LoginDto request)
+    {
+
+        if (_userService.UserLogin(request) != null)
+        {
+            var keyBytes = Encoding.ASCII.GetBytes(secretKey);
+            var claims = new ClaimsIdentity();
+            claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, request.Username));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+
+            string tokencreado = tokenHandler.WriteToken(tokenConfig);
+
+            return StatusCode(StatusCodes.Status200OK, new { token = tokencreado });
+
+        }
+        else
+        {
+
+            return StatusCode(StatusCodes.Status401Unauthorized, new { token = "" });
+        }
+    } */
+
+    [HttpPost("addrole")]
+    public async Task<IActionResult> AddRoleAsync(AddRoleDto model)
+    {
+        var result = await _userService.AddRoleAsync(model);
+        return Ok(result);
+    }
 }
--- ApiIncidencias/Services/UserService.cs
+++ ApiIncidencias/Services/UserService.cs
@@ -133,14 +133,12 @@ public class UserService : IUserService
         }
         return $"Credenciales incorrectas para el usuario {usuario.Username}.";
     }
-
-    // 
     public async Task<DatosUsuarioDto> GetTokenAsync(LoginDto model)
     {
         DatosUsuarioDto datosUsuarioDto = new DatosUsuarioDto();
         var usuario = await _unitOfWork.Usuarios
                     .GetByUsernameAsync(model.Username);
-        if (usuario == null) // Primer validacion del usuario
+        if (usuario == null)
         {
             datosUsuarioDto.EstaAutenticado = false;
             datosUsuarioDto.Mensaje = $"No existe ningún usuario con el username {model.Username}.";
@@ -148,7 +146,7 @@ public class UserService : IUserService
         }
 
         var result = _passwordHasher.VerifyHashedPassword(usuario, usuario.Password, model.Password);
-        if (result == PasswordVerificationResult.Success) // Validacion de la contraseña
+        if (result == PasswordVerificationResult.Success)
         {
 
             datosUsuarioDto.Mensaje = "Ok";
@@ -157,12 +155,14 @@ public class UserService : IUserService
             datosUsuarioDto.Email = usuario.Username;
             datosUsuarioDto.Token = _jwtGenerador.CrearToken(usuario);
             return datosUsuarioDto;
+
         }
         datosUsuarioDto.EstaAutenticado = false;
         datosUsuarioDto.Mensaje = $"Credenciales incorrectas para el usuario {usuario.Username}.";
         return datosUsuarioDto;
+
     }
-    private JwtSecurityToken CreateJwtToken(Usuario usuario)
+    private JwtSecurityToken CreateJwtToken(Usuario usuario)       
     {
         var roles = usuario.Roles;
         var roleClaims = new List<Claim>();
